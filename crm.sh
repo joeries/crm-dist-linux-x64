e9d815befbb8b2a533c0d25fde90d1e53f759a03
Joeries Young <joeries.young@gmail.com>
2025-08-03 13:10:19 +0800

    v1.0 AutoRun.

--- crm.sh
+++ crm.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-#chkconfig: 2345 80 90
-#description: crm
+### BEGIN INIT INFO
+# Provides:          crm.sh
+# Required-Start:    $network $syslog
+# Required-Stop:     $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: CRM Service
+# Description:       Start/Stop CRM Service
+### END INIT INFO
 cd /opt/crm-dit-linux-x64
 ./CRM.UI &
