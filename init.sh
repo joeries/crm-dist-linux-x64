0fa4d14a276212b6ee8751f4215d0049a46b80b2
Joeries Young <joeries.young@gmail.com>
2025-08-03 12:46:57 +0800

    v1.0 1. AutoRun.

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 chmod +w ./CRM.Log
 chmod +w ./wwwroot/Upload
 chmod +x ./CRM.UI
-cp start.sh /etc/rc.d/init.d/crm.sh
-cd /etc/rc.d/init.d
+cp start.sh /etc/init.d/crm.sh
+cd /etc/init.d
 chmod +x crm.sh
-chkconfig --add crm.sh
-chkconfig crm.sh on
-#firewall-cmd --permanent --add-port=80/tcp
-#firewall-cmd --reload
+systemctl enable crm
