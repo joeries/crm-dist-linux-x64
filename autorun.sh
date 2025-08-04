5835ef0affeca52bd076945b0457ca81d51b886b
Joeries Young <joeries.young@gmail.com>
2025-08-04 15:58:29 +0800

    v1.0 Update.

--- autorun.sh
+++
@@ -1,6 +0,0 @@
-sudo chmod +rw ./CRM.Log
-sudo chmod +rw ./DataProtection
-sudo cp crm.service /etc/systemd/system/crm.service
-sudo systemctl enable crm.service
-sudo systemctl start crm.service
-sudo systemctl status crm.service
