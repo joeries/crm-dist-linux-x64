5708a2c6b9108387964caf392462f9cd649589e5
Joeries <joeries.young@gmail.com>
2025-08-24 18:27:08 +0800

    v1.3.0 1. [fix(*)] Update autorun.sh.

--- autorun.sh
+++ autorun.sh
@@ -2,5 +2,5 @@ sudo chmod +rw ./CRM.Log
 sudo chmod +rw ./DataProtection
 sudo cp crm.service /etc/systemd/system/crm.service
 sudo systemctl enable crm.service
-sudo systemctl start crm.service
+sudo systemctl restart crm.service
 sudo systemctl status crm.service
